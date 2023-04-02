f16becc600a646f2e9f4595d97c6ca4fbab5bd2d
Vreding <105096961+Vreding@users.noreply.github.com>
2023-04-02 18:27:08 +0000

    Deploying to gh-pages from @ Vreding/goit-js-hw-09@5ac6c307f153dad166ef63c788a9904e34c52f5a 🚀

---
+++ 01-color-switcher.b387196b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");let o;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.b387196b.js.map
